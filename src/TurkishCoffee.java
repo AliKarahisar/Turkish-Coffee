918c9d890df210d26dffc486f0721aea36d4539e
Ali Karahisar <karahisarali@gmail.com>
2022-01-23 14:28:37 +0300

    v0.2 - Updates
    Main GUI removed
    Notify Icon support added
    Pop-up Menu support for taskbar and notify Icon
    Code Optimization

--- src/TurkishCoffee.java
+++ src/TurkishCoffee.java
@@ -1,122 +1,127 @@
-import com.formdev.flatlaf.FlatDarkLaf;
-import org.jetbrains.annotations.NotNull;
-
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.event.HyperlinkEvent;
-
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URI;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Objects;
 import java.util.Timer;
 import java.util.TimerTask;
 
-
 public class TurkishCoffee {
+    public static String imagePath = "assets/turkish.png";
+    public static Boolean timerStatus = false;
+    public static Desktop desktop = Desktop.getDesktop();
     static Timer myTimer;
     static TimerTask timerJob;
 
-    public static void createGui() throws IOException {
-        FlatDarkLaf.setup();
+    public static PopupMenu popupMenu() {
+        PopupMenu trayPopupMenu = new PopupMenu();
+        MenuItem timerStart = new MenuItem("Start");
+        timerStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!timerStatus) {
+                    try {
+                        createTimer();
+                        timerStart.setLabel("Stop");
+                        timerStatus = true;
+                    } catch (AWTException ex) {
+                        ex.printStackTrace();
+                    }
+                } else {
+                    timerStatus = false;
+                    timerStart.setLabel("Start");
+                    stopTimer();
+                }
+            }
+        });
+        trayPopupMenu.add(timerStart);
 
-        Desktop desktop = Desktop.getDesktop();
-        String imagePath = "assets/turkish.png";
+        MenuItem seperator = new MenuItem("-");
+        trayPopupMenu.add(seperator);
+
+        MenuItem about = new MenuItem("About");
+        about.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                createAbout();
+            }
+        });
+        trayPopupMenu.add(about);
+
+        MenuItem close = new MenuItem("Close");
+        close.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        trayPopupMenu.add(close);
+        return trayPopupMenu;
+    }
+
+    private static void createAbout() {
+        JLabel label = new JLabel();
+        Font font = label.getFont();
+
+        String style = "font-family:" +
+                font.getFamily() + ";" + "font-weight:" + (font.isBold() ? "bold" : "normal") + ";" +
+                "font-size:" + font.getSize() + "pt;";
+
+        JEditorPane ep = new JEditorPane("text/html", "<html><body style=\"" + style + "\">"
+                + "<strong>Turkish Coffee v0.2</strong><br/>Coded by, <a href=\"https://alikarahisar.com/\">Ali Karahisar</a>"
+                + "</body></html>");
+        ep.addHyperlinkListener(e1 -> {
+            if (e1.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED)) {
+                try {
+                    desktop.browse(URI.create(e1.getURL().toString()));
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        ep.setEditable(false);
+        ep.setBackground(label.getBackground());
+        JOptionPane.showMessageDialog(null, ep, "About", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void createGui() throws IOException {
+
         BufferedImage myPicture = ImageIO.read(Objects.requireNonNull(TurkishCoffee.class.getResource(imagePath)));
-        Icon appIcon = new ImageIcon(myPicture);
+        Icon appIcon = new ImageIcon(imagePath);
+        PopupMenu popupMenu = popupMenu();
+        desktop.setAboutHandler(e ->
+                createAbout()
+        );
+
+        if (SystemTray.isSupported()) {
+            SystemTray tray = SystemTray.getSystemTray();
+
+            TrayIcon icon = new TrayIcon(Toolkit.getDefaultToolkit().createImage(
+                    TurkishCoffee.class.getResource(
+                            imagePath)),
+                    "Turkish Coffee", popupMenu
+            );
+            icon.setImageAutoSize(true);
+
+            try {
+                tray.add(icon);
+            } catch (AWTException e) {
+                e.printStackTrace();
+            }
+        }
         final Taskbar taskbar = Taskbar.getTaskbar();
         try {
             taskbar.setIconImage(myPicture);
+            taskbar.setMenu(popupMenu);
         } catch (final UnsupportedOperationException | SecurityException e) {
             e.printStackTrace();
         }
-        final JFrame[] frame = new JFrame[1];
-        //System.out.println(UIManager.getLookAndFeel());
-        try {
-            UIManager.setLookAndFeel("com.formdev.flatlaf.FlatDarkLaf");
-        } catch (Exception ex) {
-            System.err.println("Failed to initialize LaF");
-        }
-
-        SwingUtilities.invokeLater(() -> frame[0] = new JFrame());
-        JPanel panel = new JPanel();
-        JPanel imagePanel = new JPanel();
-        JButton startButton = new JButton("Start");
-        JButton stopButton = new JButton("Stop");
-        JLabel lblImage = new JLabel(new ImageIcon(myPicture));
-        lblImage.setHorizontalAlignment(SwingConstants.LEFT);
-        frame[0].setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame[0].setResizable(false);
-        frame[0].setSize(400, 200);
-        frame[0].setName("Turkish Coffee");
-        frame[0].setTitle("Turkish Coffee");
-        frame[0].setIconImage(Toolkit
-                .getDefaultToolkit()
-                .getImage(String
-                        .valueOf(ImageIO.read(Objects
-                                .requireNonNull(TurkishCoffee.class.getResource("/assets/turkish.png"))))));
-        imagePanel.add(lblImage);
-        panel.add(startButton);
-        panel.add(Box.createVerticalStrut(150));
-        panel.add(stopButton);
-        frame[0].getContentPane().add(BorderLayout.LINE_START, imagePanel);
-        frame[0].getContentPane().add(BorderLayout.CENTER, panel);
-        centerWindow(frame[0]);
-        desktop.setAboutHandler(e -> {
-            JLabel label = new JLabel();
-            Font font = label.getFont();
-
-            String style = "font-family:" +
-                    font.getFamily() + ";" + "font-weight:" + (font.isBold() ? "bold" : "normal") + ";" +
-                    "font-size:" + font.getSize() + "pt;";
-
-            JEditorPane ep = new JEditorPane("text/html", "<html><body style=\"" + style + "\">"
-                    + "<strong>Turkish Coffee v0.1</strong><br/>Coded by, <a href=\"https://alikarahisar.com/\">Ali Karahisar</a>"
-                    + "</body></html>");
-
-            ep.addHyperlinkListener(e1 -> {
-                if (e1.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED)) {
-                    try {
-                        desktop.browse(URI.create(e1.getURL().toString()));
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
-                    }
-                }
-            });
-            ep.setEditable(false);
-            ep.setBackground(label.getBackground());
-            JOptionPane
-                    .showMessageDialog(null, ep, "About", JOptionPane.INFORMATION_MESSAGE, appIcon);
-        });
-
-        stopButton.setEnabled(false);
-        startButton.addActionListener(e -> {
-            startButton.setEnabled(false);
-            stopButton.setEnabled(true);
-            try {
-                createTimer();
-            } catch (AWTException ex) {
-                ex.printStackTrace();
-            }
-        });
-        stopButton.addActionListener(e -> {
-            stopButton.setEnabled(false);
-            startButton.setEnabled(true);
-            stopTimer();
-        });
-    }
-
-    private static void centerWindow(@NotNull JFrame frame) {
-        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-        int x = (int) ((dimension.getWidth() - frame.getWidth()) / 2);
-        int y = (int) ((dimension.getHeight() - frame.getHeight()) / 2);
-        frame.setLocation(x, y);
-        frame.setVisible(true);
     }
 
     private static void createTimer() throws AWTException {
@@ -135,12 +140,6 @@ public class TurkishCoffee {
     private static void stopTimer() {
         myTimer.cancel();
         timerJob.cancel();
-    }
-
-    public static String createDate() {
-        DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-        Calendar cal = Calendar.getInstance();
-        return dateFormat.format(cal.getTime());
     }
 
     public static void main(String[] args) throws IOException {
