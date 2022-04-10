98149faf75163ef563f5d7294b772be8a34db6f5
Ali Karahisar <karahisarali@gmail.com>
2022-04-10 07:07:33 +0300

    v0.5.1
    Some exception arrangements

--- src/TurkishCoffee.java
+++ src/TurkishCoffee.java
@@ -1,3 +1,5 @@
+import com.formdev.flatlaf.FlatDarkLaf;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.event.HyperlinkEvent;
@@ -8,28 +10,30 @@ import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URI;
-import java.util.*;
+import java.util.Objects;
+import java.util.ResourceBundle;
 import java.util.Timer;
+import java.util.TimerTask;
 
 import static java.awt.event.MouseEvent.BUTTON3;
 
 public class TurkishCoffee {
-    static ResourceBundle bundle = ResourceBundle.getBundle("i18n/AppUI");
+    public static final String imagePath = "assets/turkish-coffee-passive.png";
+    public static final String imagePathActive = "assets/turkish-coffee-active.png";
+    public static final Desktop desktop = Desktop.getDesktop();
+    public static final PopupMenu trayPopupMenu = new PopupMenu();
     final static Taskbar taskbar = Taskbar.getTaskbar();
+    static final ResourceBundle bundle = ResourceBundle.getBundle("i18n/AppUI");
+    public static final MenuItem timerStart = new MenuItem(bundle.getString("start"));
+    static final SystemTray tray = SystemTray.getSystemTray();
     private static final String DEFAULT_TITLE = "Turkish Coffee";
-    private static final String VERSION = "0.5.0";
+    private static final String VERSION = "0.5.1";
     private static final String VERSION_STRING = DEFAULT_TITLE + " v" + VERSION;
     private static final String URL = "https://alikarahisar.com";
     private static final String URL_STRING = "Ali Karahisar";
-    public static String imagePath = "assets/turkish-coffee-passive.png";
-    public static String imagePathActive = "assets/turkish-coffee-active.png";
     public static Boolean timerStatus = false;
-    public static Desktop desktop = Desktop.getDesktop();
-    public static PopupMenu trayPopupMenu = new PopupMenu();
-    public static MenuItem timerStart = new MenuItem(bundle.getString("start"));
     static Timer myTimer;
     static TimerTask timerJob;
-    static SystemTray tray = SystemTray.getSystemTray();
     static TrayIcon icon;
 
     public static void eventListenerForPopup() throws AWTException, IOException {
@@ -54,7 +58,13 @@ public class TurkishCoffee {
         trayPopupMenu.add(seperator);
 
         MenuItem about = new MenuItem(bundle.getString("about"));
-        about.addActionListener(e -> createAbout());
+        about.addActionListener(e -> {
+            try {
+                createAbout();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
         trayPopupMenu.add(about);
 
         MenuItem close = new MenuItem(bundle.getString("exit"));
@@ -63,7 +73,7 @@ public class TurkishCoffee {
         return trayPopupMenu;
     }
 
-    private static void createAbout() {
+    private static void createAbout() throws IOException {
         JLabel label = new JLabel();
         Font font = label.getFont();
 
@@ -85,22 +95,39 @@ public class TurkishCoffee {
                 }
             }
         });
+        Icon icon = new ImageIcon(ImageIO.read(Objects.requireNonNull(TurkishCoffee.class
+                        .getResourceAsStream(imagePathActive)))
+                .getScaledInstance(64, 64, Image.SCALE_SMOOTH));
+        icon.getIconHeight();
         ep.setEditable(false);
         ep.setBackground(label.getBackground());
-        JOptionPane.showMessageDialog(null, ep, bundle.getString("about"), JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, ep, bundle.getString("about"),
+                JOptionPane.INFORMATION_MESSAGE, icon);
     }
 
     public static void createGui() throws IOException {
-
+        FlatDarkLaf.setup();
+        try {
+            UIManager.setLookAndFeel("com.formdev.flatlaf.FlatDarkLaf");
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }
         BufferedImage myPicture = ImageIO.read(Objects.requireNonNull(TurkishCoffee.class.getResource(imagePath)));
         PopupMenu popupMenu = popupMenu();
         desktop.setAboutHandler(e ->
-                createAbout()
+                {
+                    try {
+                        createAbout();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
         );
 
         if (SystemTray.isSupported()) {
             icon = new TrayIcon(Toolkit.getDefaultToolkit().createImage(
-                    TurkishCoffee.class.getResource(imagePath)), DEFAULT_TITLE + " - "+bundle.getString("active"), popupMenu);
+                    TurkishCoffee.class.getResource(imagePath)), DEFAULT_TITLE + " - "
+                    + bundle.getString("active"), popupMenu);
             icon.setImageAutoSize(true);
 
             try {
